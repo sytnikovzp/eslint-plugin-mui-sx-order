8ad1d6c37bb4d0bba23eedce8f058bde77cbff3c
Sytnikov Aleksandr <sytnikov.zp@gmail.com>
2025-05-20 05:18:17 +0300

    Added tests

---
+++ tests/sort-sx-keys.test.js
@@ -0,0 +1,42 @@
+const { RuleTester } = require('eslint');
+const rule = require('../lib/rules/sort-sx-keys');
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    parserOptions: {
+      ecmaFeatures: { jsx: true },
+    },
+  },
+});
+
+ruleTester.run('sort-sx-keys', rule, {
+  valid: [
+    {
+      code: `const style = {
+  position: 'absolute',
+  top: 0,
+  margin: 5,
+  padding: 10,
+  backgroundColor: '#fff',
+};`,
+    },
+    {
+      code: `<Box sx={{ margin: 1, padding: 2, backgroundColor: '#eee' }} />`,
+    },
+  ],
+  invalid: [
+    {
+      code: `const style = {
+  padding: 10,
+  margin: 5,
+};`,
+      output: `const style = {
+  margin: 5,
+  padding: 10,
+};`,
+      errors: [{ messageId: 'incorrectOrder' }],
+    },
+  ],
+});
