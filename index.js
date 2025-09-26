ea5bb9494d22f3400ae046df49a02a3dd937f074
Oleksandr Sytnikov <sytnikov.zp@gmail.com>
2025-09-27 01:17:21 +0300

    fix: rollback to old version

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rules: {
+    'sort-sx-keys': require('./lib/rules/sort-sx-keys'),
+  },
+};
--- tests/sort-sx-keys.test.js
+++ tests/sort-sx-keys.test.js
@@ -1,112 +1,42 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = require("@typescript-eslint/utils");
-const sort_sx_keys_1 = __importDefault(require("../lib/rules/sort-sx-keys"));
-const ruleTester = new utils_1.TSESLint.RuleTester({
-    parser: require.resolve('@typescript-eslint/parser'),
+const { RuleTester } = require('eslint');
+const rule = require('../lib/rules/sort-sx-keys');
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
     parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: { jsx: true },
+      ecmaFeatures: { jsx: true },
     },
+  },
 });
-ruleTester.run('sort-sx-keys', sort_sx_keys_1.default, {
-    valid: [
-        {
-            code: `
-        const buttonStyle = {
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'red',
-        };
-      `,
-        },
-        {
-            code: `
-        <Box sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'blue',
-        }} />
-      `,
-        },
-        {
-            code: `
-        const styles = {
-          display: 'flex',
-          '&:hover': {
-            color: 'red',
-            backgroundColor: 'yellow',
-          }
-        };
-      `,
-        },
-        {
-            code: `
-        <Box sx={[{ display: 'flex', justifyContent: 'center', alignItems: 'center' }]} />
-      `,
-        },
-        {
-            code: `
-        const styles = {
-          display: 'flex',
-          '&:hover': {
-            backgroundColor: 'yellow',
-            color: 'red',
-          }
-        };
-      `,
-        },
-    ],
-    invalid: [
-        {
-            code: `
-        const buttonStyle = {
-          backgroundColor: 'red',
-          display: 'flex',
-          justifyContent: 'center',
-        };
-      `,
-            errors: [{ messageId: 'incorrectOrder' }],
-            output: `
-        const buttonStyle = {
-          display: 'flex',
-          justifyContent: 'center',
-          backgroundColor: 'red',
-        };
-      `,
-        },
-        {
-            code: `
-        <Box sx={{
-          backgroundColor: 'blue',
-          display: 'flex',
-          justifyContent: 'center',
-        }} />
-      `,
-            errors: [{ messageId: 'incorrectOrder' }],
-            output: `
-        <Box sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          backgroundColor: 'blue',
-        }} />
-      `,
-        },
-        {
-            code: `
-        <Box sx={[{ backgroundColor: 'blue', display: 'flex' }]} />
-      `,
-            errors: [{ messageId: 'incorrectOrder' }],
-            output: `
-        <Box sx={[{ display: 'flex', backgroundColor: 'blue' }]} />
-      `,
-        },
-    ],
+
+ruleTester.run('sort-sx-keys', rule, {
+  valid: [
+    {
+      code: `const style = {
+  position: 'absolute',
+  top: 0,
+  margin: 5,
+  padding: 10,
+  backgroundColor: '#fff',
+};`,
+    },
+    {
+      code: `<Box sx={{ margin: 1, padding: 2, backgroundColor: '#eee' }} />`,
+    },
+  ],
+  invalid: [
+    {
+      code: `const style = {
+  padding: 10,
+  margin: 5,
+};`,
+      output: `const style = {
+  margin: 5,
+  padding: 10,
+};`,
+      errors: [{ messageId: 'incorrectOrder' }],
+    },
+  ],
 });
-//# sourceMappingURL=sort-sx-keys.test.js.map
